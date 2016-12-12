09b7813bb56ca0244e728b58d799764d868ce31d
Jacob Dobson <jacobdobson@Jacob-Dobsons-MacBook-Pro.local>
2016-12-12 09:41:37 -0800

    conveting to Swift 3 and refactoring

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,31 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //outlets
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var colorSquare: UIView!
     @IBOutlet weak var openButton: UIButton!
     
+    //globals
+    let defaults = UserDefaults.standard //reference to shared default objects
+                                         //store a reference to the shared defaults object using the standardUserDefaults() method (where you store all user preferences)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        self.colorSquare.layer.borderColor = UIColor.blackColor().CGColor//add CGFloat black border to UIView
-        self.colorSquare.layer.borderWidth = 1//add border width to UIView
-        
-        // Loading from NSUser Defaults \\
-        let defaults = NSUserDefaults.standardUserDefaults()//reference to shared default objects
+        self.colorSquare.layer.borderColor = UIColor.black.cgColor //add CGFloat black border to UIView
+        self.colorSquare.layer.borderWidth = 1                     //add border width to UIView
         
         //extract the numeric value of the three colors and apply them to the sliders
-        self.redSlider.value = defaults.floatForKey("red")
-        self.greenSlider.value = defaults.floatForKey("green")
-        self.blueSlider.value = defaults.floatForKey("blue")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.redSlider.value = defaults.float(forKey: "red")
+        self.greenSlider.value = defaults.float(forKey: "green")
+        self.blueSlider.value = defaults.float(forKey: "blue")
     }
     
     @IBAction func updateBackgroundColor() {
@@ -43,20 +39,18 @@ class ViewController: UIViewController {
         let blue = CGFloat(self.blueSlider.value)
         
         self.colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
-        
-        let defaults = NSUserDefaults.standardUserDefaults()//store a reference to the shared defaults object using the standardUserDefaults() method (where you store all user preferences)
-        
+
         // store the three slider values within the shared defaults object
-        defaults.setFloat(self.redSlider.value, forKey: "red")
-        defaults.setFloat(self.greenSlider.value, forKey: "green")
-        defaults.setFloat(self.blueSlider.value, forKey: "blue")
+        defaults.set(self.redSlider.value, forKey: "red")
+        defaults.set(self.greenSlider.value, forKey: "green")
+        defaults.set(self.blueSlider.value, forKey: "blue")
         
-        defaults.synchronize()//To be sure that the data is saved immediately
+        defaults.synchronize() //To be sure that the data is saved immediately
     }
-    // opening 2nd view controller with background of colorSquare
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    //opening 2nd view controller with background of colorSquare
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "segueColor") {
-            if let ResultsViewController = segue.destinationViewController as? ResultsViewController {
+            if let ResultsViewController = segue.destination as? ResultsViewController {
                 ResultsViewController.view.backgroundColor = self.colorSquare.backgroundColor
             }
         }
